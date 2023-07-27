39afa0a1203145effbd4a1d8de217a7034a2e844
少轻狂 <1677568218@qq.com>
2023-07-27 23:59:24 +0800

    feat: test

---
+++ .github/actions-scripts/validate.js
@@ -0,0 +1,24 @@
+import {setFailed, setOutput} from '@actions/core';
+import { getRepoFromHubURL } from './utils/hub.js';
+
+function run() {
+    let { REPOSITORY: repository } = process.env
+    if (!repository) throw new Error('REPOSITORY env var is not set')
+    setFailed("test~~~~~")
+    if (repository.includes('http')) {
+        try {
+            // When parsed the url will be like <https://hub.docker.com>
+            const url = new URL(repository.replace(/[<>]/g, ''));
+            repository = getRepoFromHubURL(url);
+        } catch (e) {
+            setFailed(e);
+
+            return;
+        }
+    }
+
+
+    setOutput('repository', repository);
+}
+
+run();
