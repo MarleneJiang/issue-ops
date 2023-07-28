b997a2b5c5c49140a0d7a291af8336442579efc7
少轻狂 <1677568218@qq.com>
2023-07-28 17:11:21 +0800

    feat: add test

--- .github/actions-scripts/validate.js
+++ .github/actions-scripts/validate.js
@@ -1,20 +1,26 @@
 import {setFailed, setOutput} from '@actions/core';
+function getRepoFromHubURL(url) {
+  if (url.host !== "github.com") {
+      throw "URL must be from github.com domain";
+  }
 
+  return url;
+}
 function run() {
     let { REPOSITORY: repository } = process.env
     if (!repository) throw new Error('REPOSITORY env var is not set')
-    console.log(repository)
-    setFailed("test~~~~~")
-    if (repository.includes('http')) {
-        try {
-            // When parsed the url will be like <https://hub.docker.com>
-            const url = new URL(repository.replace(/[<>]/g, ''));
-        } catch (e) {
-            setFailed(e);
-
-            return;
-        }
-    }
+    // repository 是否含有https://github.com/字段
+    if (repository.includes('https://github.com/')) {
+      try {
+          // When parsed the url will be like <https://hub.docker.com>
+          const url = new URL(repository.replace(/[<>]/g, ''));
+          repository = getRepoFromHubURL(url);
+          console.log(repository);
+      } catch (e) {
+          setFailed(e);
+          return;
+      }
+  }
 
 
     setOutput('repository', repository);
