85a2ba7c7da2199c4bc9cb60a7ba437f2a484734
少轻狂 <1677568218@qq.com>
2023-08-17 01:52:51 +0800

    ci: 优化py

--- .github/actions_scripts/parse.py
+++ .github/actions_scripts/parse.py
@@ -3,46 +3,47 @@ from __future__ import annotations
 
 import os
 import re
-import uuid
 from pathlib import Path
-from typing import Any, Literal
+from typing import Any
 
 import requests
 
 BASE_URL = "https://pypi.org/pypi"
-CODE =404
+CODE = 404
 
-
-
-def set_multiline_output(name:str, value:str)->None:
-  """Set Github Outputs."""
-  with Path(os.environ["GITHUB_OUTPUT"]).open("a") as fh:
-    delimiter = uuid.uuid1()
-    print(f"{name}<<{delimiter}", file=fh)
-    print(value, file=fh)
-    print(delimiter, file=fh)
+def set_action_outputs(output_pairs: dict[str, str]) -> None:
+  """Sets the GitHub Action outputs, with backwards compatibility for self-hosted runners without a GITHUB_OUTPUT environment file. Keyword arguments: output_pairs - Dictionary of outputs with values."""
+  if "GITHUB_OUTPUT" in os.environ :
+    with Path(os.environ["GITHUB_OUTPUT"]).open(mode="a") as f :
+      for key, value in output_pairs.items() :
+        f.write(f"{key}={value}\n")
+  else :
+    for key, value in output_pairs.items() :
+      print(f"::set-output name={key}::{value}")  # noqa: T201
 
 def get_response(name: str) -> dict[str, Any] | None:
-  """Request response from PyPi API."""
-  target_url = f"{BASE_URL}/{name}/json"
-  response = requests.get(target_url, timeout=5)
-  if response.status_code == CODE:
-    return None
-  return response.json()
+    """Request response from PyPi API."""
+    target_url = f"{BASE_URL}/{name}/json"
+    response = requests.get(target_url, timeout=5)
+    if response.status_code == CODE:
+        return None
+    return response.json()
 
-def check_pypi(name:str)->bool:
-  """Check module filename for conflict."""
-  if (name =="null"):
+
+def check_pypi(name: str) -> bool:
+    """Check module filename for conflict."""
+    if name == "null":
+        return False
+    response = get_response(name)
+
+    if response:
+        module_name = response.get("info", {}).get("package_url", "").split("/")[-2]
+        return name.lower() == module_name.lower()
+
     return False
-  response = get_response(name)
 
-  if response:
-    module_name = response.get("info", {}).get("package_url", "").split("/")[-2]
-    return name.lower() == module_name.lower()
 
-  return False
-
-def parse_title(title:str)->dict[str, Any]:
+def parse_title(title: str) -> dict[str, Any]:
     """Prase Title."""
     pattern = r"\[(plugin|adapter|bot)\]:\s*(.+)"
     match = re.match(pattern, title)
@@ -51,34 +52,20 @@ def parse_title(title:str)->dict[str, Any]:
     msg = "标题格式错误"
     raise ValueError(msg)
 
-def main()->Literal[0]:
-  """信息解析:1. 标题的type解析,如果不符合就报错 2. 提取name、module_name、pypi_name,如果不符合就报错 3. pypi_name在pip网站中检查,不存在则报错."""
-  try:
-      title = os.environ["TITLE"]
-  except KeyError:
-    set_multiline_output("result", "error")
-    set_multiline_output("output", "Missing required input `TITLE`.")
-    return 0
-  try:
-      pypi_name = os.environ["PYPI_NAME"]
-  except KeyError:
-    set_multiline_output("result", "error")
-    set_multiline_output("output", "Missing required input `PYPI_NAME`.")
-    return 0
-  try:
-    parsed = parse_title(title)
-  except ValueError as e:
-    set_multiline_output("result", "error")
-    set_multiline_output("output", str(e))
-    return 0
-  if (check_pypi(pypi_name) is False):
-    print("解析失败~")
-    set_multiline_output("result", "error")
-    set_multiline_output("output", "输入的pypi_name存在问题")
-    return 0
-  set_multiline_output("result", "sucess")
-  set_multiline_output("output", "")
-  set_multiline_output("type", parsed.get("type",""))
-  set_multiline_output("name", parsed.get("name",""))
-  return 0
-main()
+
+def main() -> None:
+    """信息解析:1. 标题的type解析,如果不符合就报错 2. 提取name、module_name、pypi_name,如果不符合就报错 3. pypi_name在pip网站中检查,不存在则报错."""
+    title = os.environ["TITLE"]
+    pypi_name = os.environ["PYPI_NAME"]
+    try:
+        parsed = parse_title(title)
+        if check_pypi(pypi_name) is False:
+          set_action_outputs({"result": "error", "output": "输入的pypi_name存在问题"})
+        else:
+          set_action_outputs({"result": "sucess", "output": "", "type": parsed.get("type", ""), "name": parsed.get("name", "")})
+    except ValueError as e:
+        set_action_outputs({"result": "error", "output": str(e)})
+
+
+if __name__ == "__main__":
+    main()
