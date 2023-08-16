5bf5fa74d09be288c5d247ceb60eba6010394065
少轻狂 <1677568218@qq.com>
2023-08-17 02:53:04 +0800

    ci: test

--- .github/actions_scripts/main.py
+++ .github/actions_scripts/main.py
@@ -1,9 +1,11 @@
 """This module contains the main script for the GitHub actions."""
 
-from importlib.metadata import Distribution, metadata
+# from importlib.metadata import Distribution, metadata
 
-pypi_name = "alicebot"
+# pypi_name = "alicebot"
 
 
-print(Distribution.from_name(pypi_name).read_text("METADATA"))
-print(metadata(pypi_name).get_all("Project-URL"))
+# print(Distribution.from_name(pypi_name).read_text("METADATA"))
+# print(metadata(pypi_name).get_all("Project-URL"))
+
+print("a")
