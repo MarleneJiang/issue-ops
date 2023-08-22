3dd848fa6e0423ea56a682c03eb3e00d88a0c77f
少轻狂 <1677568218@qq.com>
2023-08-23 04:26:26 +0800

    ci: test

--- test.py
+++
@@ -1,9 +0,0 @@
-"""."""
-
-from importlib.metadata import metadata
-
-metadata = metadata("alicebot-plugin-template")
-
-email=(metadata.get_all("Author-email"))
-if email is not None and "<" in email[0]: # PDM发包问题
-            print(email[0].split("<")[0].strip())
