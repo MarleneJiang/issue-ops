efc89461853553f0930c65f34ff3110665fc88aa
少轻狂 <1677568218@qq.com>
2023-08-23 03:35:13 +0800

    ci: test

--- .github/actions_scripts/parse.py
+++ .github/actions_scripts/parse.py
@@ -48,7 +48,7 @@ def check_module(module_name: str) -> bool:
         importlib.reload(module)
         print(f"module_name: {module_name}")# noqa: T201
     except BaseException as e:  # noqa: BLE001
-        print(f"module_name is invalid ${e}")# noqa: T201
+        print(f"module_name is invalid {e}")# noqa: T201
         return False
     else:
         print("module_name is valid")# noqa: T201
