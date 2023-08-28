259738af91bbcbcee8583ba36c999ebd706361e5
少轻狂 <1677568218@qq.com>
2023-08-28 19:41:23 +0800

    fix: 处理依赖冲突问题

--- .github/actions_scripts/utils.py
+++ .github/actions_scripts/utils.py
@@ -56,15 +56,15 @@ class Pypi :
     def get_info(self)->dict[str,str]:
         """."""
         name = self.get("name")
-        if name is None:
+        if (name is None) or (name == ""):
             msg = "模块名称获取失败"
             raise ValueError(msg)
         description = self.get("summary")
-        if description is None:
+        if (description is None) or (description == ""):
             msg = "模块描述获取失败"
             raise ValueError(msg)
         author = self.get("author")
-        if author is None:
+        if (author is None) or (author == ""):
             email = self.get("author_email")
             if email is not None and "<" in email:  # PDM发包问题
                 author = email.split("<")[0].strip()
@@ -78,7 +78,7 @@ class Pypi :
         if homepage is None:
             homepage = ""
         tags = self.get("keywords")
-        if tags is None:
+        if (tags is None) or (tags == ""):
             msg = "标签信息获取失败"
             raise ValueError(msg)
         return {
